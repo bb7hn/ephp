b56d4429accb8069003bb4c42f88b02debbbc0a8
Batuhan Ozen <iletisim@batuhanozen.com>
2023-05-09 00:55:20 +0300

    updated /helpers.php require function

--- helpers.php
+++ helpers.php
@@ -1,25 +1,31 @@
 <?php
+
 //require configuration
 require_once 'config.php';
 
-function require_helpers($dir='helpers'){
-    foreach (glob("$dir/*") as $filename) {
-        // TODO REMEMBER TO DELETE THIS LATER
-        //echo $filename . '<hr>';
-        if(is_dir($filename)){
-            require_helpers($filename);
-        }
-        else{
-            // check if file is a PHP file
-            if (pathinfo($filename, PATHINFO_EXTENSION) == 'php') {
-                require_once $filename;
+function require_helpers(array $paths=['helpers'])
+{
+    foreach($paths as $dir) {
+        foreach (glob("$dir/*") as $filename) {
+            if(is_dir($filename)) {
+                require_helpers([$filename]);
+            } else {
+                // check if file is a PHP file
+                if (pathinfo($filename, PATHINFO_EXTENSION) == 'php') {
+                    //echo $filename.'<hr/>';
+                    require_once $filename;
+                }
             }
         }
     }
 }
 
-//require core helpers
-require_helpers('helpers/.core');
+$helper_paths = [
+    __DIR__.'/.core',
+    __DIR__.'/helpers',
+    __DIR__.'/helpers/.core',
+];
+/* var_dump($helper_paths);
+echo'<br><br><hr/>'; */
 //require helpers
-require_helpers();
-?>
+require_helpers($helper_paths);
